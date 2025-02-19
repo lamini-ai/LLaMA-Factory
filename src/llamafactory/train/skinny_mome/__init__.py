05a4381146d8bb8c5119700e054ca028a0e9f828
Lamini <lamini@Laminis-MacBook-Pro.local>
2025-02-18 18:37:37 -0800

    update

--- src/llamafactory/train/skinny_mome/__init__.py
+++ src/llamafactory/train/skinny_mome/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .workflow import run_sft
+from .workflow import run_skinny_mome
 
 
 __all__ = ["run_skinny_mome"]
